28745352bd7f068a006dbc71f71528a83d540017
Jimmy Millard <millardjimmy@gmail.com>
2020-05-23 15:03:54 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,20 +1,19 @@
-class GetRequester
+require 'open-uri'
+require 'net/http'
+require 'json'
 
-  attr_accessor :url
+class GetRequester
   def initialize(url)
     @url = url
   end
-  
+
   def get_response_body
     uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
   end
-  
-  def parse_json
-    uri = URI.parse(@url)
-    response = Net::HTTP.get_response(uri)
-    JSON.parse(response.body)
-  end
 
-end 
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
