738132b1f77367a6dd17a46da6410725aa3e2050
Jimmy Millard <millardjimmy@gmail.com>
2020-05-23 14:56:36 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,6 +4,11 @@ class GetRequester
   def initialize(url)
     @url = url
   end
-
+  
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
 
 end 
